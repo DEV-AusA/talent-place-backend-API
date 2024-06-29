b87d73eda238bbfbf05e060d35c4312bdb05895a
Cesar Ausa <cesarausaprog@gmail.com>
2024-06-29 15:21:16 -0300

    logger global implemented

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express, { NextFunction, Request, Response } from "express"
 import router from "./routes/PrincipalRouter"
-// import IError from "./interfaces/IError";
 import cors from "cors"
 import IError from "./interfaces/IError.interface";
+import { loggerGlobal } from "./middlewares/logger.middleware";
 
 const server = express();
-server.use(cors())
+server.use(cors());
 server.use(express.json());
+server.use(loggerGlobal);
 server.use(router);
 
 // manejo los errores y los muestra en .json
