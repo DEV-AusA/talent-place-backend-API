92afe3f3ce5e03cf9795721b3ecf44cf8cd5a1a7
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-01 20:59:53 -0300

    JWT Payload extende to Request, Interface to extends JwtPayload, Middleware JWT verify in 2fa/setup && 2fa/verify, logger Global implemented

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express"
 import router from "./routes/Principal.Router"
 import cors from "cors"
 import IError from "./interfaces/IError.Interface";
-import { loggerGlobal } from "./middlewares/Logger.middleware";
+import { loggerGlobal } from "./middlewares/Logger.Middleware";
 
 const server = express();
 server.use(cors());
