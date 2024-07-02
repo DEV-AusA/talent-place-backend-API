2ac11c633b36742151a376a344c4c8887ed5666f
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-02 10:16:04 -0300

    update deploy test config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,8 +11,8 @@ dotenvConfig({ path: '.env' });
 export const AppDataSource = new DataSource({
     type: "postgres",
     port: 5432,
-    host: 'postgresdb',
-    // host: process.env.DB_HOST,
+    // host: 'postgresdb',
+    host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_DATABASE,
