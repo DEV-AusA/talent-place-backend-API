25f9cc2d139d243da7513a6169439d7d06147109
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-03 20:42:48 -0300

    login & register implemented

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,9 +2,11 @@ import * as speakeasy from "speakeasy";
 import * as qrcode from "qrcode";
 import { AppDataSource } from "../config/typeorm.config";
 import IQrCodeData from "../interfaces/iQrCodeData.interface";
-import { Usuario } from "../entities/usuario";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import Usuario from "../entities/usuario";
 
-const UserRepository = AppDataSource.getRepository(Usuario);
+const userRepository = AppDataSource.getRepository(Usuario);
 
 const auth2FaSetupService = async (userId: number) => {
     const secret = speakeasy.generateSecret({
@@ -26,7 +28,7 @@ const auth2FaSetupService = async (userId: number) => {
 
 const findUserById = async (id: number) => {
     try {
-        const user = await UserRepository.findOneBy({id});
+        const user = await userRepository.findOneBy({id});
         
         if (!user) throw ({
             message: `No existe el usuario con id ${id}`,
@@ -63,7 +65,7 @@ const updateUserSecret2Fa = async (userId: Partial<Usuario>, secret: string) => 
     try {
 
         if(!userId.autenticacion2FAHabilitada) {
-            await UserRepository.update(userId, {
+            await userRepository.update(userId, {
                 autenticacion2FAHabilitada: true,
                 autenticacion2FASecreto: secret
             });            
@@ -73,8 +75,69 @@ const updateUserSecret2Fa = async (userId: Partial<Usuario>, secret: string) => 
     }
 }
 
+const createUser = async (nombre: string, contrasenia: string, email: string, tipo: string): Promise<Usuario> => {
+    try {
+      const hashedPassword = await bcrypt.hash(contrasenia, 10);
+  
+      const newUser = userRepository.create({
+        nombre,
+        contrasenia: hashedPassword,
+        email,
+        tipo,
+      });
+  
+      await userRepository.save(newUser);
+  
+      return newUser;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  const authLogin = async (email: string , contrasenia: string ) => {
+    try {
+    
+      const user = await userRepository.findOne({ where: { email } });
+      if (!user) {
+        throw {
+          message: 'No existe el usuario',
+          code: 404,
+          error: 'Usuario incorrecto'
+        };
+      }
+  
+      const passwordMatch = await bcrypt.compare(contrasenia, user.contrasenia);
+      if (!passwordMatch) {
+        throw {
+          message: 'Contraseña incorrecta',
+          code: 401,
+          error: 'Contraseña incorrecta'
+        };
+      }
+  
+      const token = jwt.sign(
+        { userId: user.id, username: user.nombre },
+        process.env.JWT_SECRET,
+        { expiresIn: '48h' }
+      );
+
+      user.updatedAt = new Date();
+      await userRepository.save(user);
+  
+      return {
+        message: 'Inicio de sesión exitoso',
+        refreshToken: token
+      };
+  
+    } catch (error) {
+      throw error;
+  } 
+};
+
 
 export default {
     auth2FaSetupService,
-    auth2FaVerifyService
+    auth2FaVerifyService,
+    createUser,
+    authLogin
 }
