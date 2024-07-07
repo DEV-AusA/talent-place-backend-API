983b8dcbf44da2f071fc902e1528ddb9e474fc06
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-07 19:48:41 -0300

    project.service structure created

---
+++ src/services/project.service.ts
@@ -0,0 +1,28 @@
+
+const getAllProjectsService = async () =>{
+
+}
+
+const getProyectByIdService = async () =>{
+
+}
+
+const postNewProjectService = async () =>{
+
+}
+
+const editProjectByIdService = async () =>{
+
+}
+
+const deleteProjectByIdService = async () =>{
+
+}
+
+export default {
+    getAllProjectsService,
+    getProyectByIdService,
+    postNewProjectService,
+    editProjectByIdService,
+    deleteProjectByIdService
+}
