87abdba80b9f5f7b84374c55ee7414a65a2ee708
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-07 19:39:52 -0300

    middlewares implemented in projects.router

--- src/routes/projects.router.ts
+++ src/routes/projects.router.ts
@@ -1,19 +1,41 @@
 import { Router } from "express";
 import projectController from "../controllers/project.controller";
 import jwtVerifyMiddleware from "../middlewares/jwtVerify.middleware";
+import jwtIdMatchVerifyMiddleware from "../middlewares/jwtIdMatchVerify.middleware";
+import jwtRolVerify from "../middlewares/jwtRolVerify.middleware";
 
 
 const projectRouter: Router = Router();
 
-projectRouter.get("/", projectController.getAllProjects)
+projectRouter.get("/",
+    projectController.getAllProjects,
+    jwtIdMatchVerifyMiddleware.jwtIdMatchVerify
+);
 
-projectRouter.get("/:id", projectController.editProject)
+projectRouter.get("/:id",
+    projectController.editProject,
+    jwtIdMatchVerifyMiddleware.jwtIdMatchVerify
+);
 
 //Para empresa
-projectRouter.post("/", jwtVerifyMiddleware.jwtVerify, projectController.postNewProject)
+projectRouter.post("/", jwtVerifyMiddleware.jwtVerify,
+    jwtIdMatchVerifyMiddleware.jwtIdMatchVerify,
+    jwtRolVerify(['empresa']),
+    projectController.postNewProject
+);
 
-projectRouter.put("/:id", jwtVerifyMiddleware.jwtVerify, projectController.editProject)
+projectRouter.put("/:id",
+    jwtVerifyMiddleware.jwtVerify,
+    jwtIdMatchVerifyMiddleware.jwtIdMatchVerify,
+    jwtRolVerify(['empresa']),
+    projectController.editProject
+);
 
-projectRouter.delete("/:id", jwtVerifyMiddleware.jwtVerify, projectController.deleteProject)
+projectRouter.delete("/:id",
+    jwtVerifyMiddleware.jwtVerify,
+    jwtIdMatchVerifyMiddleware.jwtIdMatchVerify,
+    jwtRolVerify(['empresa']),
+    projectController.deleteProject
+);
 
 export default projectRouter
