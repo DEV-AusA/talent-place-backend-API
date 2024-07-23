5295a101d9fd1873e6272527f8be1e6b9bf139a1
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-23 01:06:31 -0300

    update users Route & controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import userService from "../services/user.service";
 import catchAsync from "../utils/catchAsync.util";
-import Usuario from "../entities/usuario";
 
 const getUserProfile = async (req: Request, res: Response) =>{
-    const { id } = req.body;
+    const { id } = req.params;
     const userProfile = await userService.getUserProfileByIdService(id);
     res.status(200).json(userProfile)
 }
@@ -14,7 +13,7 @@ const editUserData = async (req: Request, res: Response) =>{
 }
 
 const getAllUsers = async (req: Request, res: Response) => {
-    const users: Usuario[] = await userService.getAllUsersService();
+    const users = await userService.getAllUsersService();
     res.status(200).json(users);
 }
 
